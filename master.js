3d2d3d2525edfd9f376131104319d451e3583846
MohamedRamdan1 <medo44448888@gmail.com>
2022-05-08 20:57:32 +0200

    21% done

--- master.js
+++ master.js
@@ -5,6 +5,9 @@ const span1 = document.getElementById("span1");
 const span2 = document.getElementById("span2");
 const span3 = document.getElementById("span3");
 
+const section2 = document.getElementById("section2");
+const movingspan = document.querySelectorAll(".graphContainer span");
+
 menuBtn.addEventListener("click", function () {
     menuContents.classList.toggle("-right-64");
     span2.classList.toggle("hidden");
@@ -28,3 +31,20 @@ menuBtn.addEventListener("click", function () {
     span3.classList.toggle("-translate-y-1/2");
 
 });
+
+window.onscroll=function(){
+    if(window.scrollY >= section2.offsetTop-400){
+        movingspan.forEach((span)=>{
+            span.style.width = span.dataset.width;
+            if (span.dataset.width <= '10%') {
+                span.style.backgroundColor = "#F2B375";
+            }
+            if ('10%' < span.dataset.width <= '50%') {
+                span.style.backgroundColor = "#DD4224";
+            }
+            if (span.dataset.width > '50%') {
+                span.style.backgroundColor = "#B41D00";
+            }
+        })
+    }
+}
