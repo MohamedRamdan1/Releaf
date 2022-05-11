2b625a9190812483c1b15a8635723c49d4995edd
MohamedRamdan1 <medo44448888@gmail.com>
2022-05-11 05:12:04 +0200

    45% done

--- master.js
+++ master.js
@@ -4,9 +4,34 @@ const menuContents = document.getElementById("menuContents");
 const span1 = document.getElementById("span1");
 const span2 = document.getElementById("span2");
 const span3 = document.getElementById("span3");
+const span4 = document.getElementById("span4");
+
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const p3 = document.getElementById("p3");
+const p4 = document.getElementById("p4");
+
+
 
 const section2 = document.getElementById("section2");
 const movingspan = document.querySelectorAll(".graphContainer span");
+const percentage = document.querySelectorAll(".percentage");
+
+window.onscroll=function(){
+    if(window.scrollY >= section2.offsetTop-300){
+        movingspan.forEach((span)=>{
+            span.style.width = span.dataset.width +"%";
+            span.innerHTML = span.dataset.width +"%";
+            if(Number(span.dataset.width) > 60) {
+                span.style.backgroundColor = "#B41D00";
+            }else if(Number(span.dataset.width)  > 20 && Number(span.dataset.width)  <= 60) {
+                span.style.backgroundColor = "#dd6524";
+            }else{
+                span.style.backgroundColor = "#F2B375";
+            }
+        })
+    }
+}
 
 menuBtn.addEventListener("click", function () {
     menuContents.classList.toggle("-right-64");
@@ -31,20 +56,3 @@ menuBtn.addEventListener("click", function () {
     span3.classList.toggle("-translate-y-1/2");
 
 });
-
-window.onscroll=function(){
-    if(window.scrollY >= section2.offsetTop-300){
-        movingspan.forEach((span)=>{
-            span.style.width = span.dataset.width;
-            if (span.dataset.width <= '10%') {
-                span.style.backgroundColor = "#F2B375";
-            }
-            if ('10%' < span.dataset.width <= '50%') {
-                span.style.backgroundColor = "#DD4224";
-            }
-            if (span.dataset.width > '50%') {
-                span.style.backgroundColor = "#B41D00";
-            }
-        })
-    }
-}
